5d0cd46bc17b7ba2746ae05e6295e2581aa86ed3
AndreQuintass <andre.quintas.t0120710@edu.atec.pt>
2023-07-21 15:23:45 +0100

    fast api guide examples

--- FastApi proj/main.py
+++ FastApi proj/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from Posts import postList
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/posts/{id}")
+async def rootTeste(id: int):
+    p = postList[id]
+    return {"id": f"{id}", "title": f"{p.title}", "body": f"{p.body}","userId":f"{p.userId}" }
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/posts")
+async def rootTeste(id: int = -1):
+    pId = 1
+    if id == -1:
+        posts = []
+        for post in postList:
+            posts.append({"id": f"{pId}", "title": f"{post.title}", "body": f"{post.body}", "userId": f"{post.userId}"})
+            pId += 1
+        return posts
+
+    p = postList[id]
+    return {"id": f"{id}", "title": f"{p.title}", "body": f"{p.body}","userId":f"{p.userId}" }
