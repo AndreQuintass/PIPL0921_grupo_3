c81203e2de4d5b4c1449c1a87456482f30d51b6d
AndreQuintass <andre.quintas.t0120710@edu.atec.pt>
2023-07-24 15:16:49 +0100

    db erro nao cria os registos

--- FastApi proj/main.py
+++ FastApi proj/main.py
@@ -7,20 +7,63 @@ app = FastAPI()
 @app.get("/setup")
 async def setup_database():
     try:
+
         conn = sqlite3.connect("pi_dbDemo.sqlite")
 
-        conn.execute("""
+        conn.executescript("""
+        
+        Create Table turmas(
+            id INTEGER primary key AUTOINCREMENT,
+            nome TEXT,
+            numero INTEGER
+        );
+        
         Create Table alunos(
-            id INTEGER primary key,
+            id INTEGER primary key AUTOINCREMENT,
             nome TEXT,
             numero INTEGER,
-            email TEXT
-        )
+            email TEXT,
+            turma_id INTEGER,
+            CONSTRAINT fk_aluno_turma
+            FOREIGN KEY (turma_id)
+            REFERENCES turmas(id)
+        );
+        
+        Create Table professores(
+            id INTEGER primary key AUTOINCREMENT,
+            nome TEXT,
+            numero INTEGER,
+            email TEXT,
+            turma_id INTEGER,
+            CONSTRAINT fk_professor_turma
+            FOREIGN KEY (turma_id)
+            REFERENCES turmas(id)
+        );
         """)
 
         conn.close()
-    except:
-        pass
+        return "success"
+    except Exception as error:
+        return str(error)
+
+@app.get("/addteste")
+async def addteste():
+    conn = sqlite3.connect("pi_dbDemo.sqlite")
+
+    conn.execute("""INSERT INTO turmas(nome, numero) VALUES ("PI0921", 10);""")
+
+    #conn.execute("""INSERT INTO alunos(nome, numero, email, turma_id) VALUES ("Sofia Lopes", 25, "sofia.lopes@edu.atec.pt", 1);""")
+    
+    #conn.execute("""INSERT INTO professores(nome, numero, email, turma_id) VALUES ("Mario Luis", 52, "mario.luis@edu.atec.pt", 1);""")
+
+    conn.commit()
+
+    conn.close()
+
+@app.get("/checkall")
+async def checkall():
+    pass
+
 
 @app.get("/")
 async def root():
